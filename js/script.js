3141954dc5dbe26db5e9d200c8dd0b8e3c1a2c67
JefferySeo <125331815+JefferySeo@users.noreply.github.com>
2023-04-09 01:23:21 +0900

    ss

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
 $(function(){
+    // 광고버튼 작동
     $('.top-adver-close').click(function(){
         $('#top-adver').fadeOut(300);
     })
+
+    // header 넣었다 뻈다
+    $('.header-gnb>li:not(:nth-child(5))>a').mouseover(function(){
+        $('.header-hide').removeClass('on');
+        $('.header-gnb>li>a').removeClass('on')
+        $('.header-gnb>li>.header-lnb').removeClass('on');
+        $('.header-hide').addClass('on');
+        $(this).addClass('on')
+        $(this).next('.header-lnb').addClass('on');
+    })
+    $('.header-hide').mouseleave(function(){
+        $('.header-hide').removeClass('on');
+        $('.header-gnb>li>a').removeClass('on')
+        $('.header-gnb>li>.header-lnb').removeClass('on');
+    })
+
+
 }); // jQuery
